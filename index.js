80135925ca9cc2b3cec0ee364b1b2d844273d3ed
danehermansen <daneherm211@gmail.com>
2022-04-27 16:51:22 -0600

    second commit

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function addMovie(event) {
 
     const movieTitle = document.createElement('span')
     movieTitle.textContent = inputField.value
+    movieTitle.addEventListener('click', crossOffMovie)
     movie.appendChild(movieTitle)
     
     const deleteBtn = document.createElement('button')
