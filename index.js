9e0d6b39d8203baff21fbe316e76a225dbf50ecf
danehermansen <daneherm211@gmail.com>
2022-04-27 16:47:39 -0600

    js-interactivity

---
+++ index.js
@@ -0,0 +1,39 @@
+console.log("javas is conec")
+
+const message = document.querySelector('#message')
+
+function addMovie(event) {
+    event.preventDefault()
+    let inputField = document.querySelector('input')
+
+    const movie = document.createElement('li')
+
+    const movieTitle = document.createElement('span')
+    movieTitle.textContent = inputField.value
+    movie.appendChild(movieTitle)
+    
+    const deleteBtn = document.createElement('button')
+    deleteBtn.textContent = 'x'
+    deleteBtn.addEventListener('click', deleteMovie)
+    movie.appendChild(deleteBtn)
+    
+    
+    
+    document.querySelector('ul').appendChild(movie)
+    
+    inputField.value = ''
+}
+document.querySelector('form').addEventListener('submit', addMovie)
+
+function deleteMovie(event) {
+    event.target.parentNode.remove()
+    message.textContent = 'Movie deleted'
+}
+function crossOffMovie(event) {
+    event.target.classList.toggle('checked')
+    if (event.target.classList.contains('checked')=== true) {
+        message.textContent = 'Movie watched'
+    } else {
+        message.textContent = 'movie added back'
+    }
+}
